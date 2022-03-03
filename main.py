02c07e3ba80791d7e20477319385f329d1120e8a
Admin <igorkydryashov7k@gmail.com>
2022-03-03 16:50:14 +0800

    upd

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import random as r
 import statistics as s
 
 if __name__ == '__main__':
-    sampleSize = 1000
+    n = 1000
 
-    selection = [3 * r.uniform(0, 1) for i in range(sampleSize)]
+    selection = [3 * r.uniform(0, 1) for i in range(n)]
 
     average = s.mean(selection)
 
@@ -13,13 +13,12 @@ if __name__ == '__main__':
 
     standardDeviation = m.sqrt(variance)
 
-    delta = 1.96 * standardDeviation / m.sqrt(sampleSize)
+    delta = 1.96 * standardDeviation / m.sqrt(n)
 
-    x1 = average - delta
-    x2 = average + delta
+    x1, x2 = average - delta, average + delta
 
-    print('Оценка математического ожидания\n', round(average, 3))
-    print('Оценка дисперсии\n', round(variance, 3))
-    print('Оценка среднеквадратического отклонения\n', round(standardDeviation, 3))
-    print('Интервальная оценка\n', '(', round(x1, 3), ',', round(x2, 3), ')')
-    print('Точечная оценка\n', round((x1 + x2) / 2, 3))
+    print('Оценка математического ожидания\n', round(average, 3), sep='')
+    print('Оценка дисперсии\n', round(variance, 3), sep='')
+    print('Оценка среднеквадратического отклонения\n', round(standardDeviation, 3), sep='')
+    print('Интервальная оценка\n', '(', round(x1, 3), ', ', round(x2, 3), ')', sep='')
+    print('Точечная оценка\n', round((x1 + x2) / 2, 3), sep='')
